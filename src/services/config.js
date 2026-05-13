d05717a1920050b4fd98c8c1f79b5e01677e770d
Azita Taheri <96353949+azitataheri@users.noreply.github.com>
2026-05-13 13:19:57 +0330

    change config file

--- src/services/config.js
+++ src/services/config.js
@@ -9,7 +9,7 @@ const api = axios.create({
 
 
 // interceptors
-api.interceptors.response.use((reponse) => reponse.data,
+api.interceptors.response.use((reponse) => reponse,
     (error) => Promise.reject(error))
 
 export {
