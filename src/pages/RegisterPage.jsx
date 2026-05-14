5dd9cb2dae783af56a7b104f0758f8f54fc225e4
Azita Taheri <96353949+azitataheri@users.noreply.github.com>
2026-05-14 13:19:29 +0330

    add register styles

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 import { registerUser } from "../services/auth";
+import styles from "../pages/RegitsterPage.module.css";
+import union from "../assets/images/union.png";
 
 function RegisterPage() {
   const [username, setUsername] = useState("");
@@ -13,7 +16,7 @@ function RegisterPage() {
   // Handle register
   const handleRegister = async () => {
     setError("");
-    
+
     if (!username || !password || !confirmpassword) {
       setError("All fields are required");
       return;
@@ -25,7 +28,7 @@ function RegisterPage() {
 
     try {
       setLoading(true);
-      const res = await registerUser( { username, password });
+      const res = await registerUser({ username, password });
       console.log(res.data);
       navigate("/login");
     } catch (error) {
@@ -36,31 +39,41 @@ function RegisterPage() {
     }
   };
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="نام کاربری"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
+    <div className={styles.container}>
+      <h3 className={styles.mainTitle}>بوت کمپ بوتواستارت</h3>
+      <div className={styles.registerForm}>
+        <img src={union} alt="refister logo" />
+        <p className={styles.registerTitle}>فرم ثبت نام</p>
+        <form onSubmit={handleRegister}>
+          <input
+            type="text"
+            placeholder="نام کاربری"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-      <input
-        type="password"
-        placeholder=" رمز عبور"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+          <input
+            type="password"
+            placeholder=" رمز عبور"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-      <input
-        type="password"
-        placeholder="تکرار رمز عبور"
-        value={confirmpassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-      />
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <button onClick={handleRegister} disabled={loading}>
-        {loading ? "loading..." : "Register"}
-      </button>
+          <input
+            type="password"
+            placeholder="تکرار رمز عبور"
+            value={confirmpassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          {error && <p style={{ color: "red" }}>{error}</p>}
+          <div>
+            <button type="submit" disabled={loading}>
+              {loading ? "loading..." : "ثبت نام"}
+            </button>
+            <small>حساب کاربری دارید؟</small>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
